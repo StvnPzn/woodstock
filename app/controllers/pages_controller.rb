c06b439ec3edf043f1c1bdf511e234813fb276dc
Lisa Chatellier <78473267+shinboram-90@users.noreply.github.com>
2021-05-26 17:45:54 +0200

    FAT MODAAAAAAAL

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    # tables = Category.where(title: "table")
-    # commodes = Category.where(title: "commode")
-    # chaises = Category.where(title: "chaise")
-    # buffets = Category.where(title: "buffet")
-    # bibliotheques = Category.where(title: "bibliothèque")
-    # meuble_tvs = Category.where(title: "meuble tv")
+    tables = Category.where(title: "table")
+    commodes = Category.where(title: "commode")
+    chaises = Category.where(title: "chaise")
+    buffets = Category.where(title: "buffet")
+    bibliotheques = Category.where(title: "bibliothèque")
+    meuble_tvs = Category.where(title: "meuble tv")
   end
 end
