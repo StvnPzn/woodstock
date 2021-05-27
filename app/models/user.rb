218be5e1658d4d18cb3ce6eb287c3845086ce457
flore bayle <flore.bayle@gmail.com>
2021-05-27 11:06:00 +0200

    edit-revised

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :pieces 
+  has_many :pieces
   has_many :orders
 
   enum role: { craftman: 0, client: 1 }
