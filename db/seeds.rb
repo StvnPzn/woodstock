db60918825a62417ec5f861185201b44b95eeb22
Lisa Chatellier <78473267+shinboram-90@users.noreply.github.com>
2021-05-28 09:30:21 +0200

    view de pieces index en place

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 
 puts "destroy all"
 Category.destroy_all
@@ -23,26 +24,36 @@ puts "done"
 puts "creating tables with their parts"
 
 table1 = Piece.create!(name: "Didi", user: lisa, category: table)
+file = URI.open("https://www.monmeubletendance.com/clients/myhometendance/documents/1280_1024/pr_MA88CA492TABLSXT3I-0ec7fc1c56-43.jpg")
+table1.photo.attach(io: File.open(file), filename: 'table.jpg', content_type: 'image/jpg')
 
 top_table1 = Part.create!(color: 2, height: 10, width: 100, length: 160, material: 1, shape: 1, position: 0, piece: table1)
 bottom_table1 = Part.create!(color: 1, height: 100, width: 60, length: 0, material: 4, shape: 1, position: 1, piece: table1)
 
 table2 = Piece.create!(name: "Loulou", user: lisa, category: table)
+file = URI.open("https://www.ikea.com/fr/fr/images/products/ekedalen-table-extensible-brun-fonce__0736963_pe740827_s5.jpg?f=m")
+table2.photo.attach(io: File.open(file), filename: 'table.jpg', content_type: 'image/jpg')
 
 top_table2 = Part.create!(color: 4, height: 8, width: 60, length: 100, material: 4, shape: 1, position: 0, piece: table2)
 botttom_table2 = Part.create!(color: 1, height: 100, width: 60, length: 0, material: 5, shape: 1, position: 1, piece: table2)
 
 table3 = Piece.create!(name: "Jack", user: lisa, category: table)
+file = URI.open("https://images-na.ssl-images-amazon.com/images/I/71xhMu-ehSL._AC_SY450_.jpg")
+table3.photo.attach(io: File.open(file), filename: 'table.jpg', content_type: 'image/jpg')
 
 top_table3 = Part.create!(color: 1, height: 6, width: 60, length: 120, material: 2, shape: 1, position: 0, piece: table3)
 botttom_table3 = Part.create!(color: 1, height: 80, width: 60, length: 0, material: 2, shape: 1, position: 1, piece: table3)
 
 table4 = Piece.create!(name: "Go", user: lisa, category: table)
+file = URI.open("https://www.idmarket.com/11659/table-a-manger-phoenix-224-cm-bois-et-noir.jpg")
+table4.photo.attach(io: File.open(file), filename: 'table.jpg', content_type: 'image/jpg')
 
 top_table4 = Part.create!(color: 0, height: 10, width: 60, length: 80, material: 4, shape: 1, position: 0, piece: table4)
 botttom_table4 = Part.create!(color: 4, height: 120, width: 60, length: 0, material: 5, shape: 1, position: 1, piece: table4)
 
 table5 = Piece.create!(name: "Lizzie", user: lisa, category: table)
+file = URI.open("https://www.piguno.com/workshop49035/wp-content/uploads/2018/02/Aston-coffee-table-1200x900.jpg")
+table5.photo.attach(io: File.open(file), filename: 'table.jpg', content_type: 'image/jpg')
 
 top_table5 = Part.create!(color: 5, height: 4, width: 60, length: 180, material: 2, shape: 1, position: 0, piece: table5)
 botttom_table5 = Part.create!(color: 2, height: 120, width: 60, length: 0, material: 5, shape: 1, position: 1, piece: table5)
